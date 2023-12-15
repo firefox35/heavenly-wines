53ccc2ba3ce232a43a98aaa18873965aac6e1a4b
firefox35 <f1firefox@yahoo.ie>
2023-12-15 18:52:13 +0000

    Remove issue with links

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,8 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
+#'DEVELOPMENT' in os.environ
 
 LOGGING = {
     'version': 1,
