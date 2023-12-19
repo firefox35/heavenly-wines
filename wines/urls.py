12d9f8f3be9283f234f0108e94cd43e2f73cc2ae
firefox35 <f1firefox@yahoo.ie>
2023-12-19 22:51:57 +0000

    Fixed urls on Wines

--- wines/urls.py
+++ wines/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", index.as_view(), name="home"),
     path("about/", about.as_view(), name="about"),
-    path("wines/", AddWine.as_view(), name="add_wine"),
-    path("wines", Wines.as_view(), name="wines"),
+    path("add_wines/", AddWine.as_view(), name="add_wine"),
+    path("wines/", Wines.as_view(), name="wines"),
     path("<slug:pk>/", WineDetail.as_view(), name="wine_detail"),
     path("delete/<slug:pk>/", DeleteWine.as_view(), name="delete_wine"),
     path("edit/<slug:pk>/", EditWine.as_view(), name="edit_wine"),
